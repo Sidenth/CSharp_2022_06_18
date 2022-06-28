e69ba00294601d8fd126f59e9ba90a2ff4da9e2c
Dan <nautilus.psihodelic@yandex.ru>
2022-06-28 22:23:19 +0900

    Домашнее задание второй семинар

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное 
+//трёхзначное число и удаляет вторую цифру этого числа.
+void FillArray(int [] array)
+{
+    int index = 0;
+    int length = array.Length;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int [] array2)
+{
+    int index2 = 0;
+    int length2 = array2.Length;
+    while (index2<length2)
+    {
+        Console.Write(array2[index2]);
+        index2++;
+    }
+    Console.WriteLine(" ");
+}
+int [] mass = new int[3];
+FillArray(mass);
+Console.WriteLine("Первоначальное число ");
+PrintArray(mass);
+mass[1]=mass[2];
+Console.WriteLine("Конечное число");
+Console.WriteLine($"{mass[0]}"+$"{mass[1]}");
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+string num = Console.ReadLine();
+char [] mass = num.ToCharArray();
+if (mass.Length>=3)
+{
+ Console.WriteLine($"Третья цифра числа {mass[2]}");
+}
+else
+{
+Console.WriteLine("Третьей цифры нет " );
+}
