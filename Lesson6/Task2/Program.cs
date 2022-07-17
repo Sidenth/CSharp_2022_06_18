6f8b30ad3149854d2f0aafb59b8f773c5b8099b3
Dan <nautilus.psihodelic@yandex.ru>
2022-07-17 16:16:13 +0900

    Домашнее задание шестой семинар

--- Lesson6/Task2/Program.cs
+++ Lesson6/Task2/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿/*Задача 2: Напишите программу, которая найдёт точку пересечения
+ двух прямых, заданных уравнениями y = k1 * x + b1, 
+ y = k2 * x + b2; значения b1, k1, b2 и k2 задаются 
+ пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5);*/
+void CrossPoint(int k1, int k2, int b1, int b2)
+{
+    if (k1 == b1)
+    {
+        if (k2 == b2)
+        {
+            Console.WriteLine("Прямые совпадают");
+        }
+        if (k2 != b2)
+        {
+            Console.WriteLine("Прямые параллельны");
+        }
+
+    }
+    else
+    {
+        int x = (b2 - b1) / (k1 - k2);
+        int y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+        Console.WriteLine($"Координаты точки пересечения х = {x}, y = {y})");
+    }
+
+}
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int k1 = Prompt("Введите коэффициент k1 ");
+int k2 = Prompt("Введите коэффициент k2 ");
+int b1 = Prompt("Введите коэффициент b1 ");
+int b2 = Prompt("Введите коэффициент b2 ");
+CrossPoint(k1, k2, b1, b2);
