3ed9dcb33e5b962d3be47ac67669d4668c65adaa
Dan <nautilus.psihodelic@yandex.ru>
2022-07-23 19:48:30 +0900

    Домашнее задание девятый семинар

---
+++ Lesson9/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 2: Задайте значения M и N. Напишите программу, которая найдёт
+сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30*/
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine());
+}
+int PrintSum(int M, int N)
+{
+    if (N == M)
+    {
+        return 0;
+    }
+    int sum = M + 1;
+    return PrintSum(M + 1, N) + sum;
+}
+int M = Prompt("Введите первое число ");
+int N = Prompt("Введите первое число ");
+if (M > N || M < 0 || N < 0)
+{
+    Console.WriteLine("Неверные условия ввода");
+}
+else
+{
+    int result = PrintSum(M - 1, N);
+    Console.WriteLine($"Сумма равна {result}");
+}
